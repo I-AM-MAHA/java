b98d5c9b836de21a9c06a54a107eb77b979a119a
I-AM-MAHA <149134103+I-AM-MAHA@users.noreply.github.com>
2024-11-26 17:05:03 +0530

    Add files via upload

---
+++ Sumofdigits.java
@@ -0,0 +1,26 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Sumofdigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Input an integer between 0 and 1000: ");
+        int number = scanner.nextInt();
+        
+        if (number < 0 || number > 1000) {
+            System.out.println("Invalid input. Please enter a number between 0 and 1000.");
+            return;
+        }
+        
+        int sum = 0;
+        
+        while (number > 0) {
+            sum += number % 10; 
+            number /= 10;      
+        }
+        
+        System.out.println("The sum of all digits is " + sum);
+    }
+}
